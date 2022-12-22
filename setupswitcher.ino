2815da3ae6286c1e9fb45fc6fa0ac9220dde2a06
SparkAgness <anabol1984@yandex.ru>
2022-12-22 05:30:18 +0300

    in setup_menu(item) added zeroing item, added description of item_switcher(item)

--- setupswitcher.ino
+++ setupswitcher.ino
@@ -2,7 +2,7 @@
 #define decrem PINy
 
 const uint8_t item_quant = 6; // the quantity of setup menu's items
-uint8_t item = 0; //item's counter
+uint8_t item = 0; //item's counter (year, mm, dd etc...)
 uint16_t year, mm, dd, hh, minut, sec;
 
 uint16_t switcher(uint16_t clock) { //item's value switcher
@@ -22,10 +22,22 @@ uint16_t switcher(uint16_t clock) { //item's value switcher
 bool setup_menu(uint8_t item) {
     if ((item >= 0) && (item <= item_quant)) && ((!decrem) && (!increm))
         return true;
-    if (item > item_quant) && ((!increm) && (!decrem))
+    if (item > item_quant) && ((!increm) && (!decrem)) {
+	item = 0;
         return false;
+	}
 }
 
-uint16_t setup_input(uint16_t clock, *func()) { // to learn how to refer func to func
-    
+uint8_t item_switcher(uint8_t item) {
+//switches item's value
+if ((!decrem) && (!decrem))
+    item += 1;
+return item;
+
+}
+uint16_t setup_input(uint16_t clock, uint16_t (*func)(uint16_t)) { 
+//the func of setup input 
+//*func = switcher(clock)
+
+
 }
