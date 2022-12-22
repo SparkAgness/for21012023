7549976afb596372c0845472235702f4fb2bd7e7
SparkAgness <anabol1984@yandex.ru>
2022-12-22 10:25:57 +0300

    Func setup_input has filled
    M.b. this function have to return void???

--- setupswitcher.ino
+++ setupswitcher.ino
@@ -3,17 +3,17 @@
 
 const uint8_t item_quant = 6; // the quantity of setup menu's items
 uint8_t item = 0; //item's counter (year, mm, dd etc...)
-uint16_t year, mm, dd, hh, minut, sec;
+uint16_t year, week_day, month, dd, hh, minut, sec; //sync with values of  standart library
 
 uint16_t switcher(uint16_t clock) { //item's value switcher
     println(clock);
     if (!decrem) {
         clock -= 1;
-	println(clock);
+	Serial.println(clock);
     }
     if (!increm) {
         clock += 1;
-	println(clock);
+	Serial.println(clock);
     }
     return clock;
 }
@@ -30,14 +30,24 @@ bool setup_menu(uint8_t item) {
 
 uint8_t item_switcher(uint8_t item) {
 //switches item's value
-if ((!decrem) && (!decrem))
-    item += 1;
-return item;
+    if ((!decrem) && (!decrem))
+        item += 1;
+    return item;
 
 }
 uint16_t setup_input(uint16_t clock, uint16_t (*func)(uint16_t)) { 
 //the func of setup input 
 //*func = switcher(clock)
-
+    if (setup_menu(item)) {
+        switch(item) {
+            case 1: switcher(hh);
+	    case 2: switcher(minut);
+	    case 3: switcher(year);
+            case 4: switcher(month);
+	    case 5: switcher(week_day);
+	default 
+            exit(0);	    
+	}
+    }
 
 }
