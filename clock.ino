cb003cfb57ef3eab66bef66d06b3875c7d2bf7bd
SparkAgness <anabol1984@yandex.ru>
2022-12-26 09:28:18 +0300

    upload editsetsw.ino
    add menu_switch opportunity

--- clock.ino
+++ clock.ino
@@ -4,9 +4,14 @@ MicroDS3231 rtc
 uint8_t sec, minute, hour, week_day, day, month;
 uint16_t year;
 
-uint8_t menu_switcher() {
+uint8_t menu_switcher() { //switch to setup clock menu
     uint8_t menu_switcher = 0;
     if (!increm && !decrem && millis() - time_enter > anti_bounce) {
+	menu_switcher += 1;
         time_enter = millis();
+        if (menu_switcher == 6) {
+            menu_switcher = 0;
+	}
     }
+    return menu_switcher;
 }
--- editsetsw.ino
+++ editsetsw.ino
@@ -2,9 +2,29 @@
 #define decrem_pin 5
 #define anti_bounce 500
 
+#include <microDS3231.h>
+MicroDS3231 rtc;
+
+uint8_t menu_switcher();
+
 bool increm, decrem;
 uint16_t clock;
 uint32_t time_enter; //always will be in global-value!!!
+uint8_t sec, minute, hour, week_day, day, month;
+uint16_t year;
+uint8_t m_switcher;
+
+uint8_t menu_switcher(uint8_t *m_switcher) { //switch to setup clock menu
+    
+    if (!increm && !decrem && millis() - time_enter > anti_bounce) {
+	      *m_switcher += 1;
+        time_enter = millis();
+        if (*m_switcher == 6) {
+            *m_switcher = 0;
+	}
+    }
+    return *m_switcher;
+}
 
 
 uint16_t switcher(uint16_t *clck) { 
@@ -34,5 +54,11 @@ void loop() {
 increm = digitalRead(increm_pin);
 decrem = digitalRead(decrem_pin);
  
-switcher(&clock);
+//switcher(&clock);
+menu_switcher(&m_switcher);
+Serial.println(m_switcher);
+if (!m_switcher) {
+  Serial.println("False!");
+}
+delay(1000);
 }
