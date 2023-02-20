f61a3f03594c5aa2a0d904616d1e7ec35e4c530b
SparkAgness <anabol1984@yandex.ru>
2023-02-20 05:01:02 +0300

    setup terminal is debugged
    try to do with uint8_t in time-variables vs uint16_t in terminal
    setup

--- terminal_setup.ino
+++ terminal_setup.ino
@@ -18,46 +18,41 @@ void setting_terminal() {
     if (Serial.available()) {
         tmp = Serial.readString();
     }
-    if (tmp == "hour\n") {    //terminal setup hours
-        if (millis() - time > 3000) {
+    if (tmp == "hour\n") {
+        while (!Serial.available()) {
             Serial.println("Input hours");
-            time = millis();
         }
         if (fill_from_Serial(&hour) < 0) {
             tmp.remove(0);
         }
     }
     if (tmp == "minute\n") { //terminal setup minutes
-        if (millis() - time > 3000) {
+	while (!Serial.available()) {
             Serial.println("Input minutes");
-            time = millis();
-        }
+	}
         if (fill_from_Serial(&minute) < 0) {
             tmp.remove(0);
         }
     }
     if (tmp == "day\n") { // terminal setup days
-        if (millis() - time > 3000) {
+        while(!Serial.available()) {
             Serial.println("Input day");
-            time = millis();
         }
         if (fill_from_Serial(&day) < 0) {
             tmp.remove(0);
         }
     }
     if (tmp == "month\n") { // terminal setup months
-        if (millis() - time > 3000) {
+        while(!Serial.available()) {
             Serial.println("Input month");
-            time = millis();
         }
         if (fill_from_Serial(&month) < 0) {
             tmp.remove(0);
         }
     }
     if (tmp == "year\n") { //terminal setup years
-        if (millis() - time > 3000) {
+        while(!Serial.available()) {
             Serial.println("Input year");
-            time = millis();
         }
         if (fill_from_Serial(&year) < 0) {
             tmp.remove(0);
@@ -65,10 +60,7 @@ void setting_terminal() {
     }
     
     if (millis() - time2 > 3000) { // debugging writing 
-        Serial.print("Time: ");
-        Serial.print(hour);
-        Serial.print(":"); 
-        Serial.println(minute);
+        Serial.print("Time: " + String(hour) + ":" + String(minute) + "\t");
         Serial.println("Date: " + String(day) + "." + String(month) + "." + String(year));
         time2 = millis();
     }
